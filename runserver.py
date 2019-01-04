b9ffe83da3860da67c838e14952ac53ee46f274e
lechen36 <273003635@qq.com>
2019-01-04 18:38:59 +0800

    修改
    Signed-off-by: lechen36 <273003635@qq.com>

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 """
-This script runs the FlaskWebProject1 application using a development server.
+This script runs the FlaskWebProject1 application using a development server.1
 """
 from DAC import app
 app.run()
